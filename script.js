2f6cfbb95e3390343614b504b7f5b2e20b6af6f2
aisirimshetty119 <aisirimshetty1@gmail.com>
2026-06-26 18:13:12 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Fade-in animation
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+sections.forEach(section => {
+    observer.observe(section);
+});
